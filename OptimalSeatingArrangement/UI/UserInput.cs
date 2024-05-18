29031898ada7a2ccc76ede6d48eb1643df707478
Gustav Polhem <gustavpolhem@gmail.com>
2024-05-18 13:11:07 +0200

    Add project files.

---
+++ OptimalSeatingArrangement/UI/UserInput.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptimalSeatingArrangement.UI
+{
+    public class UserInput
+    {
+
+        public async Task Menu()
+        {
+            bool closeApp = false;
+
+            Console.Clear();
+
+            while (!closeApp) 
+            {
+
+            }
+        }
+    }
+}
